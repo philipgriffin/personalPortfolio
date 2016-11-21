0d12c9d23384e8f3ed19abe196c8c8541c905fa5
Phil <philipgriffin88@gmail.com>
2016-11-21 19:19:31 +0000

    added contact section

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -4,16 +4,19 @@
 
             $stateProvider
                 .state('about', {
+                    sticky: true,
                     url: '/about',
                     template: '<pg-about></pg-about>'
                 })
 
                 .state('development', {
+                    sticky: true,
                     url: '/work',
                     template: '<pg-dev></pg-dev>'
                 })
 
                 .state('contact', {
+                    sticky: true,
                     url: '/contact',
                     template: '<pg-contact></pg-contact>'
                 });
